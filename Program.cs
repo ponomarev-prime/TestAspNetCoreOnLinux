2c85f93a3ff27d0e6c4a6b5165710907fd7a5df8
Alex <ax.dev@yahoo.com>
2023-03-14 18:30:44 +0300

    Theme changes

--- Program.cs
+++ Program.cs
@@ -9,17 +9,17 @@ app.MapGet("/", async context =>
             <title>My First ASP.NET Core Web App</title>
             <style>
                 body {
-                    background-color: #f5f5f5;
+                    background-color: #1E1E1E;
                     font-family: Arial, sans-serif;
                     font-size: 16px;
                 }
                 h1 {
-                    color: #333;
+                    color: #FFFFFF;
                     text-align: center;
                     margin-top: 50px;
                 }
                 p {
-                    color: #666;
+                    color: #2E2E2E;
                     text-align: center;
                     margin-top: 30px;
                 }
