6c5b1564ebaa0c80270e9ff862d38306b9063e58
Alex <ax.dev@yahoo.com>
2023-03-14 19:09:22 +0300

    add image

--- Program.cs
+++ Program.cs
@@ -3,39 +3,15 @@ var app = builder.Build();
 
 app.MapGet("/", async context =>
 {
-    string html = @"<!DOCTYPE html>
-        <html>
-        <head>
-            <title>My First ASP.NET Core Web App</title>
-            <style>
-                body {
-                    background-color: #1E1E1E;
-                    color: #FFFFFF;
-                    font-family: Arial, sans-serif;
-                    font-size: 16px;
-                }
-                a {
-                    color: #0099FF;
-                    text-decoration: none;
-                }
-                a:hover {
-                    color: #FFFFFF;
-                    background-color: #2E2E2E;
-                }
-                .accent {
-                    color: #FFA500;
-                }
-                .border {
-                    border: 1px solid #4D4D4D;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Hello, world!</h1>
-            <p>Welcome to my first ASP.NET Core web app!</p>
-        </body>
-        </html>";
+    string html = File.ReadAllText("wwwroot/index.html");
     await context.Response.WriteAsync(html);
+});
+
+app.MapGet("wwwroot/aspnet.png", async context =>
+{
+    string imagePath = "wwwroot/aspnet.png";
+    var imageBytes = File.ReadAllBytes(imagePath);
+    await context.Response.Body.WriteAsync(imageBytes);
 });
 
 app.Run();
